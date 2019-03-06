2e226bf993b11205df8cce996af2ef898538858b
spectre-phantom <spectre0phantom@gmail.com>
2019-03-07 01:58:43 +0530

    Made a few minor changes to new_design.cpp. Still confused with a bit of lane_change function

--- COP290_2/new_design.cpp
+++ COP290_2/new_design.cpp
@@ -1,16 +1,87 @@
 #include <tuple>
 #include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
 //The Road class with functions to handle all the updates related to the status of pixels on the road
 //everywhere in tuple, in accessign arrays first coordinate denotes y distance from top to bottom
 //second coordinate denotes x distance from left to right
 //length is along x axis, width is along y axis
+
+//Pixels with attributes to get related vehicles
+
+
+
+///////WARNING: To do {
+    /* Making sure that when a vehicle moves ahead the pixel values behindd it are set to zero if there is no vehicle behind it */
+
+class pixel
+{
+public:
+  char display;
+  Vehicle *v = NULL;
+
+public:
+  pixel()
+  {
+    this->display=" ";
+  }
+
+  //To set the vehicle when pixel gets occupied
+  void set_v(Vehicle *vehicle)
+  {
+    this->v = vehicle;   //Is this the correct way to assign the pointer
+    if ( v ) // if v is not null
+    {
+        this->display = *v.display;
+    }
+    else
+    {
+        this->display = " ";
+    }
+  }
+};
+
+class Vehicle
+{
+private:
+  int length;
+  int width;
+  char display;
+  string color;
+  string type;
+
+public:
+  tuple<int,int> upper_right;  //gives the position of its upper right corner on pixel map
+  //int speed;                For Future Use
+  //int acc;
+
+public:
+  Vehicle(int l, int w, tuple<int,int> pos, string c, string t, char d)
+  {
+    this->length = l;
+    this->width = w;
+    this->upper_right = pos;
+    this->color = c;
+    this->type = t;
+    this->display = d;
+  }
+
+  void update(tuple<int,int> pos)
+  {
+    upper_right = pos;
+  }
+
+
+};
+
 class Road
 {
 public:
   int road_length;
   int road_width;
   int signal; //0 for red, 1 for green
-  pixel road_map [road_width][road_length];
+  vector<vector<pixel>> road_map; //Changes made.
 
 public:
   //Constructor
@@ -18,20 +89,13 @@ public:
   {
     this->road_length = l;
     this->road_width = w;
-    for (int i = 0; i<l; i++)
-    {
-      for(int j = 0; j<w; j++)
-      {
-        this->road_map[j][i] = pixel();
-      }
-    }
+    this->road_map.resize(w, vector<pixel>(l, pixel())); //Changes made.
     this->signal = s;
-
   }
 
   void next_second()
   {
-    for (int i = road_length-1; i>-1; i--)
+    for (int i = road_length-1; i>=0; i--)
     {
       vector<Vehicle> considered_v;
       for (int j = 0; j<road_width; j++)
@@ -41,12 +105,12 @@ public:
           tuple<int,int> temp = (this->road_map[j][i].v->upper_right);
           if (temp == make_tuple(j,i))
           {
-            considered_v.push_back(this->road_map[j][i].v);
+            considered_v.push_back(this->road_map[j][i].v); //Pushing all valid vehicles on the road into this list.
           }
         }
       }
 
-      std::random_shuffle(considered_v.begin(), considered_v.end());
+      random_shuffle(considered_v.begin(), considered_v.end());
 
       for (int k = 0; k<considered_v.size(); k++)
       {
@@ -64,18 +128,17 @@ public:
 
   }
 
-  int move_front(tuple<int,int> upper_right, int length, int width)
+  int move_front(tuple<int,int> upper_right, int width)
   {
-    bool ans = 1;
+    int ans = 1;
     for (int j=0; j<width; j++)
     {
-      if (road_map[get<0>(upper_right)+j][get<1>(upper_right)+1].v==NULL)
+      if (road_map[get<0>(upper_right)+j][get<1>(upper_right)+1].v) //Changes made.
       {
         ans = 0;
         break;
       }
     }
-
     return ans;
     // 1 for yes, move front 0 for no, can't move move_front
     // int because lane_change will have three options 0-no, 2-left(up along y axis), 1-right(down along y axis)
@@ -83,14 +146,14 @@ public:
 
   int lane_change(tuple<int,int> upper_right, int length, int width)//only one pixel top or bottom
   {
-    bool ans = 1;
+    int ans = 1;
     //tuple<int,int> down_right = make_tuple(get<0>(upper_right)+length-1,get<1>(upper_right));
     int a = get<0>(upper_right);
     int b = get<1>(upper_right);
 
     for (int j = 0; j < width; j++)
     {
-      if (road_map[a+1+j][b+1].v!=NULL)
+      if !(road_map[a+1+j][b+1].v)
       {
         ans = 2;
         break
@@ -100,10 +163,10 @@ public:
     {
       for (int i=0; i<length; i++)
       {
-        if (road_map[a+width][b+1-i].v!=NULL)
+        if !(road_map[a+width][b+1-i].v)
         {
           ans = 2;
-          break;
+          break;    //Confusing
         }
       }
     }
@@ -202,67 +265,5 @@ public:
   {
 
   }
-
-};
-
-
-//Pixels with attributes to get related vehicles
-class pixel
-{
-public:
-  char display;
-  Vehicle *v = NULL;
-
-public:
-  pixel()
-  {
-    this->display=" ";
-  }
-
-  //To set the vehicle when pixel gets occupied
-  void set_v(Vehicle *v)
-  {
-    this->v = v;   //Is this the correct way to assign the pointer
-    if (v==NULL)
-    {
-      this->display = " ";
-    }
-    else
-    {
-      this->display = *v.display;
-    }
-  }
-};
-
-class Vehicle
-{
-private:
-  int length;
-  int width;
-  char display;
-  string color;
-  string type;
-
-public:
-  tuple<int,int> upper_right;  //gives the position of its upper right corner on pixel map
-  //int speed;                For Future Use
-  //int acc;
-
-public:
-  Vehicle(int l, int w, tuple<int,int> pos, string c, string t, char d)
-  {
-    this->length = l;
-    this->width = w;
-    this->upper_right = pos;
-    this->color = c;
-    this->type = t;
-    this->display = d;
-  }
-
-  void update(tuple<int,int> pos)
-  {
-    upper_right = pos;
-  }
-
 
 };
