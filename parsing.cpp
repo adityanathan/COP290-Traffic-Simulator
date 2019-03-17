b50f36223ebc963ed0d446986c0e5860b9a0af1e
PoorvaGarg <40417664+PoorvaGarg@users.noreply.github.com>
2019-03-17 17:27:07 +0530

    Add files via upload

---
+++ parsing.cpp
@@ -0,0 +1,133 @@
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "/Users/poorvagarg/Downloads/rapidxml-1.13/rapidxml.hpp"
+
+using namespace rapidxml;
+using namespace std;
+
+vector<vector<string > > parser(string config)
+{
+	vector < vector < string> > ans;
+	cout << "Start" << endl;
+	xml_document<> doc;
+	xml_node<> * root_node;
+	// Read the xml file into a vector
+	ifstream theFile (config);
+	vector<char> buffer((istreambuf_iterator<char>(theFile)), istreambuf_iterator<char>());
+	buffer.push_back('\0');
+	// Parse the buffer using the xml file parsing library into doc
+	doc.parse<0>(&buffer[0]);
+	// Find our root node
+	root_node = doc.first_node("road");
+
+	int length = stoi(root_node->first_attribute("length")->value());
+	int width = stoi(root_node->first_attribute("width")->value());
+	int dist = stoi(root_node->first_attribute("sig_dist")->value());
+
+	//Road road = Road(length,width,dist);
+
+
+	string initial_color = ((root_node->first_node("signal"))->first_attribute("color")->value());
+	int t_signal; //0 for red, 1 for green
+
+	if (strcmp(initial_color, "green")==0)
+	{
+		t_signal = 1;
+	}
+	else
+	{
+		t_signal = 0;
+	}
+
+	vector<int> pos(2,0);
+
+	vector<int> sig_time;
+	vector<vector <Vehicle> > sig_vehicles;
+
+	for (xml_node<> * signal_node = root_node->first_node("signal"); signal_node; signal_node = signal_node->next_sibling())
+	{
+			sig_time.push_back(signal_node->first_attribute("end")->value());
+			vector<Vehicles> sig_v;
+
+	    for(xml_node<> * vehicle_node = signal_node->first_node("vehicle"); vehicle_node; vehicle_node = vehicle_node->next_sibling())
+	    {
+				Vehicle v;
+				int len = stoi(vehicle_node->first_node("length")->value());
+				int wid = stoi(vehicle_node->first_node("width")->value());
+				string col = vehicle_node->first_node("color")->value();
+				int max_v = stoi(vehicle_node->first_node("max_v")->value());
+				int acc = stoi(vehicle_node->first_node("acc")->value());
+				char disp = vehicle_node->first_node("display")->value().at(0);
+				int id = vehicles.size();
+
+				v = Vehicle(road, len, wid, col, max_v, acc, disp, id, pos);
+				sig_v.push_back(v);
+	    }
+			sig_vehicles.push_back(sig_v);
+	}
+
+	for (int i = 0; i< sig_vehicles.size(); i++)
+	{
+		random_shuffle(sig_vehicles[i].begin(),sig_vehicles[i].end());
+	}
+
+
+
+	int next_x = 0;
+	int next_y = 1;
+	int max_length = 0;
+	for (int i=0; i<sig_vehicles.size(); i++)
+	{
+		for (int j = 0; j < sig_vehicles[i].size(); j++)
+		{
+
+			if (next_y + sig_vehicles[i][j].get_width() >= road.get_width())
+			{
+				next_y = 0;
+				next_x = next_x -max_length-1;
+				max_length = sig_vehicles[i][j].get_length();
+				sig_vehicles[i][j].set_pos(next_x, next_y);
+				next_y = next_y + sig_vehicles.get_width() +1;
+			}
+			else
+			{
+				sig_vehicles[i][j].set_pos(next_x, next_y);
+				next_y = next_y + sig_vehicles.get_width() +1;
+				if (sig_vehicles[i][j].get_length() > max_length)
+				{
+					max_length = sig_vehicles[i][j].get_length();
+				}
+			}
+
+		}
+	}
+
+	vector< vector <Vehicle> > on_road;
+	for (int i = 0; i < sig_vehicles.size(); i++)
+	{
+		for (int j = 0; j< sig_vehicles.size(); j++)
+		{
+			on_road.push_back(sig_vehicles[i][j]);
+		}
+
+		for (int k = 0; k< sig_time[i]; k++)
+		{
+			update();
+		}
+
+		t_signal = 1 - t_signal;
+
+	}
+
+}
+
+int main(int argc, char *argv[])
+{
+	string b = "green";
+	string a = b;
+	cout<<(a=="green")<<endl;;
+}
